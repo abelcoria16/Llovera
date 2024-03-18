c3d5e6c0b1058a2b428f9862d399ea4519f8d2f0
David Cedeño <DavidCT280@gmail.com>
2024-03-18 15:59:11 +0100

    actualizada api

--- js/llovera.js
+++ js/llovera.js
@@ -1,4 +1,18 @@
 'use strict';
+function ubicacion() {   
+    let longitud
+    let latitud
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition( posicion => {
+            longitud = posicion.coords.longitude
+            latitud = posicion.coords.latitude
+
+            const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&hourly=rain`
+            console.log(url);
+        })
+    }
+}
 
 function obtenerNombrePoblacion() {
     if (navigator.geolocation) {
@@ -6,18 +20,15 @@ function obtenerNombrePoblacion() {
             const latitud = position.coords.latitude;
             const longitud = position.coords.longitude;
             
-    
             const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitud}&lon=${longitud}&format=json`;
 
-           
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
-                    
+
                     const poblacion = data.address.city || data.address.town || data.address.village || data.address.hamlet || data.address.locality || data.address.county || data.address.state || data.address.country;
                     
-                   
-                    console.log("Estás en:", poblacion);
+                    console.log("TIempo en:", poblacion);
                 })
                 .catch(error => {
                     console.error('Error al obtener el nombre de la población:', error);
