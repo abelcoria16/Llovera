8b01abefc18061d0415a3639b2422d45da216588
David Cedeño <DavidCT280@gmail.com>
2024-03-18 15:56:49 +0100

    actualizado api

--- js/llovera.js
+++ js/llovera.js
@@ -1,18 +1,31 @@
 'use strict';
 
-function ubicacion() {   
-        let longitud
-        let latitud
+function obtenerNombrePoblacion() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const latitud = position.coords.latitude;
+            const longitud = position.coords.longitude;
+            
+    
+            const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitud}&lon=${longitud}&format=json`;
 
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition( posicion => {
-                longitud = posicion.coords.longitude
-                latitud = posicion.coords.latitude
-
-                const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&hourly=rain`
-                console.log(url);
-            })
-        }
+           
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    
+                    const poblacion = data.address.city || data.address.town || data.address.village || data.address.hamlet || data.address.locality || data.address.county || data.address.state || data.address.country;
+                    
+                   
+                    console.log("Estás en:", poblacion);
+                })
+                .catch(error => {
+                    console.error('Error al obtener el nombre de la población:', error);
+                });
+        });
+    } else {
+        console.error('Geolocalización no es soportada en este navegador.');
+    }
 }
 
-ubicacion();
+obtenerNombrePoblacion();
