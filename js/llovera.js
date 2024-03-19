56a630f38fa9fd5d22ed6b47efa527e02085af9e
Alberto Jimenez <alberjghoyos@gmail.com>
2024-03-19 19:22:20 +0100

    d

--- js/llovera.js
+++ js/llovera.js
@@ -3,132 +3,190 @@
 function inicioBoton() {
   const btnLluviaHoras = document.getElementById("botonTiempo");
   btnLluviaHoras.addEventListener("click", (event) => {
-      event.preventDefault();
-      obtenerLatitudLongitud();
+    event.preventDefault();
+    obtenerLatitudLongitud();
   });
 }
 
 function obtenerLatitudLongitud() {
-    let longitud;
-    let latitud;
+  let longitud;
+  let latitud;
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((posicion) => {
-            longitud = posicion.coords.longitude;
-            latitud = posicion.coords.latitude;
-            obtenerTiempoMeteorologico(latitud, longitud);
-            obtenerNombrePoblacion(latitud, longitud);
-        });
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((posicion) => {
+      longitud = posicion.coords.longitude;
+      latitud = posicion.coords.latitude;
+      obtenerTiempoMeteorologico(latitud, longitud);
+      obtenerNombrePoblacion(latitud, longitud);
+    });
+  }
 }
 
 function obtenerTiempoMeteorologico(latitud, longitud) {
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&hourly=rain&forecast_days=2`;
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            const arrayHoras = data.hourly.time;
-            const arrayLuvia = data.hourly.rain;
-            const arrayLlovera8Horas = [];
-            const array8Horas = [];
-            const hora = horaActual();
-            const hora8 = hora + 8;
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&hourly=temperature_2m,rain,cloud_cover&daily=sunrise,sunset&forecast_days=2`;
 
-            for (let i = hora; i < hora8; i++) {
-                arrayLlovera8Horas.push(arrayLuvia[i]);
-                array8Horas.push(arrayHoras[i]);
-            }
-            comprobarSiLlovera(arrayLlovera8Horas);
-            mostrarTiempo(arrayLlovera8Horas, array8Horas);
-        })
-        .catch((error) => {
-            console.error("Error al obtener el tiempo: ", error);
-        });
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const arrayHoras = data.hourly.time;
+      const arrayLuvia = data.hourly.rain;
+      const arrayTemperatura = data.hourly.temperature_2m;
+      const arrayClima = data.hourly.cloud_cover;
+      const amanecer = data.daily.sunrise[1].split("T")[1].split(":")[0];
+      const atardecer = data.daily.sunset[1].split("T")[1].split(":")[0];
+
+      const array8Horas = [];
+      const arrayLlovera8Horas = [];
+      const arrayTemperatura8Horas = [];
+      const arrayClima8Horas = [];
+
+      const hora = horaActual();
+      const hora8 = hora + 8;
+
+      for (let i = hora; i < hora8; i++) {
+        array8Horas.push(arrayHoras[i]);
+        arrayLlovera8Horas.push(arrayLuvia[i]);
+        arrayTemperatura8Horas.push(arrayTemperatura[i]);
+        arrayClima8Horas.push(arrayClima[i]);
+      }
+      comprobarSiLlovera(arrayLlovera8Horas);
+      mostrarTiempo(
+        array8Horas,
+        arrayLlovera8Horas,
+        arrayTemperatura8Horas,
+        arrayClima8Horas,
+        amanecer,
+        atardecer
+      );
+    })
+    .catch((error) => {
+      console.error("Error al obtener el tiempo: ", error);
+    });
 }
+
+function horaActual() {
+  return new Date().getHours();
+}
+
 function obtenerNombrePoblacion(latitud, longitud) {
   const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitud}&lon=${longitud}&format=json`;
 
   fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-          const poblacion =
-              data.address.city ||
-              data.address.town ||
-              data.address.village ||
-              data.address.hamlet ||
-              data.address.locality ||
-              data.address.county ||
-              data.address.state ||
-              data.address.country;
+    .then((response) => response.json())
+    .then((data) => {
+      const poblacion =
+        data.address.city ||
+        data.address.town ||
+        data.address.village ||
+        data.address.hamlet ||
+        data.address.locality ||
+        data.address.county ||
+        data.address.state ||
+        data.address.country;
 
-          mostrarPoblacion(poblacion); 
-      })
-      .catch((error) => {
-          console.error("Error al obtener el nombre de la población:", error);
-      });
+      mostrarPoblacion(poblacion);
+    })
+    .catch((error) => {
+      console.error("Error al obtener el nombre de la población:", error);
+    });
 }
 
 function comprobarSiLlovera(arrayLluvia8Horas) {
-    let llovera = false;
+  let llovera = false;
 
-    arrayLluvia8Horas[3]=0.10;
-    // arrayLluvia8Horas[7]=0.10;
+  arrayLluvia8Horas[3] = 0.1; //simular lluvia
+  // arrayLluvia8Horas[7]=0.10;
 
-    for (const lluvia of arrayLluvia8Horas) {
-        if (lluvia > 0) {
-            llovera = true;
-            break;
-        }
+  for (const lluvia of arrayLluvia8Horas) {
+    if (lluvia > 0) {
+      llovera = true;
+      break;
     }
+  }
 
-    mostrarSiLlovera(llovera);
+  mostrarSiLlovera(llovera);
 }
 
 function mostrarPoblacion(poblacion) {
-  const poblacionElement = document.getElementById('poblacion');
+  const poblacionElement = document.getElementById("poblacion");
   poblacionElement.textContent = `Estás en: ${poblacion}`;
 }
 
-function mostrarSiLlovera(llovera){
-  const parrafoLloveraSN = document.getElementById('lloveraSN');
+function mostrarSiLlovera(llovera) {
+  const parrafoLloveraSN = document.getElementById("lloveraSN");
   let lluvia;
-  if (llovera===true) {
-    lluvia = "Sí"
-  }else{
-    lluvia = "No"
+  if (llovera === true) {
+    lluvia = "Sí";
+  } else {
+    lluvia = "No";
   }
-  parrafoLloveraSN.innerHTML = `${lluvia} llovera`
+  parrafoLloveraSN.innerHTML = `${lluvia} llovera en las proximas 8 horas`;
 }
 
+function mostrarTiempo(
+  arrayHoras,
+  arrayLluvia,
+  arrayTemperaturas,
+  arrayClima,
+  amanecer,
+  atardecer
+) {
+  const container = document.getElementById("cuadrosTiempo");
+  container.style.display = "block";
+  container.innerHTML = "";
 
-function mostrarTiempo(arrayLluvia, arrayHoras) {
-    const container = document.getElementById('cuadrosTiempo');
-    container.style.display = "block";
-    container.innerHTML = ''; 
+  const weatherContainer = document.createElement("div");
+  weatherContainer.classList.add("contenedorTiempo");
 
-    const weatherContainer = document.createElement('div');
-    weatherContainer.classList.add('contenedorTiempo');
+  for (let i = 0; i < arrayLluvia.length; i++) {
+    const weatherCard = document.createElement("div");
+    weatherCard.classList.add("tiempoCuadro");
 
-    for (let i = 0; i < arrayLluvia.length; i++) {
-        const weatherCard = document.createElement('div');
-        weatherCard.classList.add('tiempoCuadro');
-        const hora = arrayHoras[i].split("T")[1];
-        // const willRain = arrayLluvia[i] > 0 ? 'Sí' : 'No';
-        // <p>Lloverá: ${willRain}</p>
-        const precipitation = arrayLluvia[i] + ' mm';
+    const hora = arrayHoras[i].split("T")[1];
 
-        weatherCard.innerHTML = `
+    const iconoTiempo = seleccionarIconoTiempo(
+      arrayClima[i],
+      arrayLluvia[i],
+      amanecer,
+      atardecer,
+      hora.split(":")[0]
+    );
+
+    
+    // const willRain = arrayLluvia[i] > 0 ? 'Sí' : 'No';
+    // <p>Lloverá: ${willRain}</p>
+    const precipitation = arrayLluvia[i] + " mm";
+
+    weatherCard.innerHTML = `
+            <img src="./img/${iconoTiempo}" alt="imagen tiempo" class="imgTiempo">
             <p>${hora}h</p>
             <p>Precipitación: ${precipitation}</p>
         `;
-        weatherContainer.appendChild(weatherCard);
-    }
+    weatherContainer.appendChild(weatherCard);
+  }
 
-    container.appendChild(weatherContainer);
+  container.appendChild(weatherContainer);
 }
 
-function horaActual() {
-    return new Date().getHours();
+function seleccionarIconoTiempo(nubes, lluvia, amanecer, atardecer, hora) {
+  let imagen = "";
+  //  nubes = 30;
+  console.log(nubes+": "+hora+": "+lluvia);
+  if (lluvia > 0) {
+    imagen = "lluvia.png";
+  } else if (nubes > 70) {
+    imagen = "nubes.png";
+  } else if (nubes > 20 && nubes<70) {
+    imagen = "solNubes.png";
+  } else if (hora < amanecer || hora > atardecer) {
+    imagen = "noche.png";
+  } else if (nubes > 20 && nubes<70 && hora < amanecer || hora > atardecer) {
+    imagen = "nocheNubes.png";
+  }else {
+    imagen = "soleado.png";
+  }
+
+  return imagen;
 }
 
 inicioBoton();
